3edca3910fb1d6c67c0f464ebe19b4f0ac563fce
Jesse Schneider <mechawrench@users.noreply.github.com>
2022-10-31 19:19:57 -0700

    RTB Retry WIP

--- app/Filament/Pages/RealtimeBattles.php
+++ app/Filament/Pages/RealtimeBattles.php
@@ -38,6 +38,8 @@ class RealtimeBattles extends Page
 
     public $invite_code;
 
+    public $guest_connected = false;
+
     public function mount(): void
     {
         $this->user_coms = auth()->user()->wifiDevices()->get();
@@ -122,34 +124,6 @@ class RealtimeBattles extends Page
         $mqtt_acl->topic = strtolower($model->user->name).'/f/'.$model->topic;
         $mqtt_acl->user_id = auth()->id();
         $mqtt_acl->save();
-
-        $message_data = [
-            'digirom' => null,
-            'application_id' => 1,
-            'hide_output' => false,
-            'topic' => $model->topic,
-            'topic_action' => 'subscribe',
-            'battle_type' => $model->device_type, // TODO: Change the model column to battle_type instead of device_type
-            'user_type' => 'guest',
-            'host' => $model->user->name,
-        ];
-
-        // Lookup WifiDevice by UUID
-        $wifi_device = auth()->user()->wifiDevices()->where('uuid', $model->opponent_com_uuid)->first();
-
-        // Send MQTT message to device
-        // TODO: Make mqtt setup a function
-        $mqtt = new \PhpMqtt\Client\MqttClient(config('mqtt-client.connections.default.host'), config('mqtt-client.connections.default.port'));
-
-        $connectionSettings = (new \PhpMqtt\Client\ConnectionSettings)
-            ->setUsername(config('mqtt-client.connections.default.connection_settings.auth.username'))
-            ->setPassword(config('mqtt-client.connections.default.connection_settings.auth.password'))
-            ->setConnectTimeout(3)
-            ->setUseTls(true)
-            ->setTlsSelfSignedAllowed(true);
-
-        $mqtt->connect($connectionSettings, true);
-        $mqtt->publish(strtolower(auth()->user()->name).'/f/'.auth()->user()->uuid.'-'.$wifi_device->uuid.'/wificom-input', json_encode($message_data));
 
         $this->guestAccept();
 
