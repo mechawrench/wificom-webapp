215a64c75aa25c226b2ab59d338e81695090c0c7
Shift <shift@laravelshift.com>
2023-07-20 02:03:12 +0000

    Shift cleanup

--- app/Http/Middleware/AuthenticateWithApiKey.php
+++ app/Http/Middleware/AuthenticateWithApiKey.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Middleware;
 
-use Symfony\Component\HttpFoundation\Response;
 use Closure;
 use Illuminate\Contracts\Auth\Factory as Auth;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthenticateWithApiKey
 {
