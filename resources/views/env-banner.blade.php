04827f4a17884bdeca9a867c052303f2f1458803
mechawrench <mechawrench@users.noreply.github.com>
2023-11-24 18:26:22 -0800

    Update banner copy and styling

--- resources/views/env-banner.blade.php
+++ resources/views/env-banner.blade.php
@@ -1,6 +1,8 @@
-<div class="bg-orange-500 text-center p-10">
-    <h2 class="text-white text-4xl">Warning: You are not in the production environment.</h2>
+<div class="bg-orange-500 text-center p-3">
+    <h2 class="text-white text-4xl">Warning: You are not in the production environment</h2>
     <h3 class="text-red-200 text-2xl font-bold">Environment: {{ env('APP_ENV') }}</h3>
-    <p class="text-white text-lg"><i>Please note that the database resets periodically, and the wificom secrets.py file must be updated for the new MQTT server.</i></p>
-    <p class="text-white text-lg"><i>Also, remember that apps created or edited here won't be reflected on the production website.</i></p>
+    <p class="text-white text-lg"><i>Please note that the database resets periodically, and the wificom secrets.py file must be updated for the new MQTT server</i></p>
+    <p class="text-white text-lg"><i>Any changes on this site will not be reflected on the production site</i></p>
+    <p class="text-white text-lg"><i>Apps on this site will not work unless the app is also connected to this site</i></p>
+.</i></p>
 </div>
