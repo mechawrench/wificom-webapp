0293560e6e782b781960bc8e729616aa73b5b209
mechawrench <mechawrench@users.noreply.github.com>
2023-11-24 22:17:37 -0800

    Update sidebar layout styles

--- resources/views/vendor/filament/components/layouts/app/sidebar/index.blade.php
+++ resources/views/vendor/filament/components/layouts/app/sidebar/index.blade.php
@@ -4,21 +4,21 @@
         x-cloak
         x-bind:class="
             $store.sidebar.isOpen
-                ? 'filament-sidebar-open translate-x-0 max-w-[20em] shadow-2xl lg:max-w-[var(--sidebar-width)]'
-                : '-translate-x-full lg:translate-x-0 lg:max-w-[var(--collapsed-sidebar-width)] lg:shadow-2xl rtl:lg:-translate-x-0 rtl:translate-x-full'
+                ? 'filament-sidebar-open translate-x-0 max-w-[20em] shadow-2xl lg:max-w-[var(--sidebar-width)] mt-16'
+                : '-translate-x-full lg:translate-x-0 lg:max-w-[var(--collapsed-sidebar-width)] lg:shadow-2xl rtl:lg:-translate-x-0 rtl:translate-x-full mt-16'
         "
     @else
         x-cloak="-lg"
         x-bind:class="
             $store.sidebar.isOpen
-                ? 'filament-sidebar-open translate-x-0 shadow-2xl'
-                : '-translate-x-full lg:translate-x-0 lg:shadow-2xl rtl:lg:-translate-x-0 rtl:translate-x-full'
+                ? 'filament-sidebar-open translate-x-0 shadow-2xl mt-16'
+                : '-translate-x-full lg:translate-x-0 lg:shadow-2xl rtl:lg:-translate-x-0 rtl:translate-x-full mt-16'
         "
     @endif
     @class([
         'filament-sidebar fixed inset-y-0 left-0 z-20 flex h-screen w-[var(--sidebar-width)] flex-col overflow-hidden bg-white transition-all rtl:left-auto rtl:right-0 lg:z-0 lg:border-r rtl:lg:border-l rtl:lg:border-r-0 transform-gpu translate-y-40',
         'lg:translate-x-0' => ! config('filament.layout.sidebar.is_collapsible_on_desktop'),
-        'dark:border-gray-700 dark:bg-gray-800' => config('filament.dark_mode'),
+        'dark:border-gray-700 dark:bg-gray-800 mt-16' => config('filament.dark_mode'),
     ])
 >
     <header
