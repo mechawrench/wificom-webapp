34ca403fe535dc8cb627452792a924bad491c8ba
mechawrench <mechawrench@users.noreply.github.com>
2023-11-26 14:27:33 -0800

    Update env-banner and sidebar styling

--- resources/views/env-banner.blade.php
+++ resources/views/env-banner.blade.php
@@ -4,7 +4,7 @@
     <div class="mx-auto sm:w-full" style="resize: both; overflow: auto;">
         <h2 class="hidden sm:hidden md:block text-white text-3xl">Warning: You are not in the production environment</h2>
         <h3 class="text-red-200 text-xl font-bold">Environment: {{ env('APP_ENV') }}</h3>
-        <p class="text-white text-base sm:text-sm md:text-sm"><i>Please note that the database resets periodically, and the wificom secrets.py file must be updated for the new MQTT server</i></p>
+        <p class="text-white text-base sm:text-base md:text-base"><i>Please note that the database resets periodically, and the wificom secrets.py file must be updated for the new MQTT server</i></p>
         <p class="hidden sm:hidden md:block text-white text-base"><i>Any changes on this site will not be reflected on the production site</i></p>
         <p class="hidden sm:hidden md:block text-white text-base"><i>Apps on this site will not work unless the app is also connected to this site</i></p>
     </div>
--- resources/views/vendor/filament/components/layouts/app/sidebar/index.blade.php
+++ resources/views/vendor/filament/components/layouts/app/sidebar/index.blade.php
@@ -7,7 +7,7 @@
             : '-translate-x-full lg:translate-x-0 lg:max-w-[10rem] lg:shadow-2xl rtl:lg:-translate-x-0 rtl:translate-x-full'
     "
     @class([
-        'filament-sidebar z-20 flex h-screen w-[var(--sidebar-width)] flex-col overflow-hidden bg-white transition-all rtl:left-auto rtl:right-0 lg:z-0 lg:border-r rtl:lg:border-l rtl:lg:border-r-0 transform-gpu',
+        'filament-sidebar z-20 flex h-full w-[var(--sidebar-width)] flex-col overflow-hidden bg-white transition-all rtl:left-auto rtl:right-0 lg:z-0 lg:border-r rtl:lg:border-l rtl:lg:border-r-0 transform-gpu',
         'lg:translate-x-0' => ! config('filament.layout.sidebar.is_collapsible_on_desktop'),
         'dark:border-gray-700 dark:bg-gray-800' => config('filament.dark_mode'),
         'fixed inset-y-0 left-0 lg:static' => config('filament.layout.sidebar.is_collapsible_on_desktop'),
