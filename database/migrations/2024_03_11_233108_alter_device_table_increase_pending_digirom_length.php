9adb74200c32eae090f9d505997c4cfa78d5f085
Mechawrench <mechawrench@users.noreply.github.com>
2024-03-11 16:39:03 -0700

    Increase pending_digirom max length, add validation (#29)
    * Increase pending_digirom max length, add validation
    
    * Increase pending_digirom max length, add validation

---
+++ database/migrations/2024_03_11_233108_alter_device_table_increase_pending_digirom_length.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        Schema::table('wifi_devices', function (Blueprint $table) {
+            $table->string('pending_digirom', 700)->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('wifi_devices', function (Blueprint $table) {
+            $table->string('pending_digirom', 255)->change();
+        });
+    }
+};
